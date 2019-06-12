3e843c0c0bc8437435fd749fbcf3ddc16524aee6
Joseph Romano <jdromano2@gmail.com>
2019-06-12 15:48:43 -0400

    Update information pages

--- src/components/Planning.jsx
+++ src/components/Planning.jsx
@@ -6,13 +6,36 @@ const Planning = ({}) => {
     <div>
       <Container text textAlign='left'>
         <Header as='h2'>
+          Weekend Schedule
+        </Header>
+        <ul>
+          <li><b>Saturday, August 10</b></li>
+          <ul>
+            <li><u>2:00pm-3:30pm:</u> Wedding (<i>St. Peter's Church, Rosendale</i>)</li>
+            <li><u>5:30pm-6:30pm:</u> Cocktail Hour (<i>Senate House grounds, Kingston</i>)</li>
+            <li><u>6:30pm-10:30pm:</u> Reception (<i>Senate Garage, Kingston</i>)</li>
+            <li><u>10:30pm-?:</u> After-Party (<i>TBD, Uptown Kingston</i>)</li>
+          </ul>
+          <li><b>Sunday, August 11</b></li>
+          <ul>
+            <li><u>10:00am-12:00pm:</u> Farewell Brunch (<i>Deising's Bakery, Kingston</i>)</li>
+          </ul>
+        </ul>
+
+        <Divider />
+        <Header as='h2'>
           Accomodations
         </Header>
         <Header as='h4'>
           Best Western Plus
         </Header>
         <p>
-          We have secured rooms at The Best Western Plus, conveniently located only 0.7 miles from the Senate Garage. You can reserve a room at a discounted rate by calling the hotel or using the link below to reserve online:
+          We have secured rooms at The Best Western Plus, conveniently located
+          only 0.7 miles from the Senate Garage. You can reserve a room at a
+          discounted rate by calling the hotel or using the link below to
+          reserve online (please note that rooms are subject to availability and
+          the discounted rate may not be available close to the date of the
+          wedding):
         </p>
         <p>
           <a href="https://www.bestwestern.com/en_US/book/hotel-rooms.33159.html?groupId=K44SS2E9" target='__blank'>https://www.bestwestern.com/en_US/book/hotel-rooms.33159.html?groupId=K44SS2E9</a>
@@ -21,7 +44,8 @@ const Planning = ({}) => {
           Airbnb
         </Header>
         <p>
-          There are many Airbnb options in Uptown Kingston and the surrounding areas. Click on the following link to search current availability (note that some listings may not be posted until closer to the wedding):
+          There are many Airbnb options in Uptown Kingston and the surrounding
+          areas. Click on the following link to search current availability:
         </p>
         <p>
         <a href="https://goo.gl/wYXeYa" target='__blank'>https://goo.gl/wYXeYa</a>
@@ -96,11 +120,60 @@ const Planning = ({}) => {
 
         <Divider />
         <Header as='h2'>
-          Wedding transportation
+          Transportation and Parking
+        </Header>
+        <Header as='h4'>
+          Wedding (St. Peter's Church)
         </Header>
         <p>
-          Transportation will be provided between the Best Western Plus and both the ceremony venue and the reception venue. Guests staying at the Best Western Plus may also find it convenient to walk to/from the reception. Check back as the date approaches for more information on timing, pickup and drop-off locations, and more!
+          Transportation will be provided between the Best Western Plus and both
+          the ceremony venue and the reception venue. Guests staying at the Best
+          Western Plus may also find it convenient to walk to/from the
+          reception. Since seating on the bus will be limited, we encourage
+          guests to drive to St. Peter's Church if they are able. Information
+          regarding locations and timing is shown below!
         </p>
+        <p>
+          Guests who opt to drive to the wedding can park at St. Peter's Church,
+          in either of the two lots adjacent to the church. If necessary, some
+          parking is also available on the right side of Keator Avenue, going up
+          the hill to the left of, and behind, the church.
+        </p>
+        <Header as='h4'>
+          Reception (Senate Garage)
+        </Header>
+        <p>
+          Like mentioned above, a bus will be provided, making trips between the
+          Best Western Plus and the Senate Garage. For timing, please refer to
+          the information below.
+        </p>
+        <p>
+          Parking at the Senate Garage may be somewhat more limited, so we
+          suggest walking (convenient from most parts of Uptown Kingston,
+          including the Best Western Plus) or carpooling when possible. Some
+          parking spaces may be available in the small lot behind the Senate
+          House or along the streets (metered) near the venue.
+        </p>
+        <Header as='h4'>
+          Farewell Brunch (Deising's Bakery)
+        </Header>
+        <p>
+          Deising's Bakery has one parking lot that
+          will likely fill up quickly, but streetside parking is also available.
+          Like for the Senate Garage, Deising's is located within easy walking
+          distance of the Best Western Plus, as well as most other locations in
+          Uptown Kingston.
+        </p>
+        <Header as='h3'>
+          Bus Schedule (Saturday, August 10)
+        </Header>
+        <ul>
+          <li><u>1:15 PM</u> - Depart Best Western Plus for St. Peter's Church</li>
+          <li><u>3:30 PM</u> - Depart St. Peter's Church returning to Best Western Plus</li>
+          <li><u>5:00 PM</u> - Depart Best Western Plus for Senate Garage</li>
+          <li><u>9:00 PM to 10:30 PM</u> - Depart Senate Garage to Best Western Plus (<b>every 30 minutes</b>)</li>
+          <li><u>12:00 PM</u> - Final departure, Senate Garage to Best Western Plus</li>
+        </ul>
       </Container>
     </div>
   );
